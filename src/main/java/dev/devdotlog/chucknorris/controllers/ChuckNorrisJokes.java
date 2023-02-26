e9bda741d51daca769a557646971dd8a1a3b993f
Leole Nkhatho <leolenkhatho@gmail.com>
2023-02-26 16:29:45 +0200

    added in controller

--- src/main/java/dev/devdotlog/chucknorris/controllers/ChuckNorrisJokes.java
+++ src/main/java/dev/devdotlog/chucknorris/controllers/ChuckNorrisJokes.java
@@ -1,5 +1,19 @@
 package dev.devdotlog.chucknorris.controllers;
 
-public class ChuckNorrisJokes {
+import dev.devdotlog.chucknorris.services.ChuckNorrisJokesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class ChuckNorrisJokes {
+    private final ChuckNorrisJokesService chuckNorrisJokesService;
+    public ChuckNorrisJokes(ChuckNorrisJokesService chuckNorrisJokesService) {
+        this.chuckNorrisJokesService = chuckNorrisJokesService;
+    }
+    @RequestMapping({"/", ""})
+    public String getChuckNorrisJoke(Model model) {
+        model.addAttribute("joke", chuckNorrisJokesService.getRandomJoke());
+        return "index";
+    }
 }
